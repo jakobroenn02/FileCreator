73104605867ba55df75d0f263960586f55ac4101
Jakob <66146289+jakobroenn02@users.noreply.github.com>
2021-08-13 20:45:41 +0200

    Add files via upload

--- IPHACK.cpp
+++ IPHACK.cpp
@@ -19,6 +19,29 @@ wstring GetUserDesktopPath()
 }
 int main()
 {   
+    cout << R"(           __    _                                   
+                    _wr""        "-q__                             
+                 _dP                 9m_     
+               _#P                     9#_                         
+              d#@                       9#m                        
+             d##                         ###                       
+            J###                         ###L                      
+            {###K                       J###K                      
+            ]####K      ___aaa___      J####F                      
+        __gmM######_  w#P""   ""9#m  _d#####Mmw__                  
+     _g##############mZ_         __g##############m_               
+   _d####M@PPPP@@M#######Mmp gm#########@@PPP9@M####m_             
+  a###""          ,Z"#####@" '######"\g          ""M##m            
+ J#@"             0L  "*##     ##@"  J#              *#K           
+ #"               `#    "_gmwgm_~    dF               `#_          
+7F                 "#_   ]#####F   _dK                 JE          
+]                    *m__ ##### __g@"                   F          
+                       "PJ#####LP"                                 
+ `                       0######_                      '           
+                       _0########_                                   
+     .               _d#####^#####m__              ,              
+      "*w_________am#####P"   ~9#####mw_________w*"                  
+          ""9@#####@M""           ""P@#####@M"")" << "\n";
     wstring currentDesktop = GetUserDesktopPath();
     Sleep(4000);
     wstring lastname = L"/suprise.txt";
@@ -28,7 +51,13 @@ int main()
         cout << "File creation failed";
         return 0;
     }
-    myfile << "I like cake";
+    myfile << R"(
+                 ##########################
+                 #                        #
+                 #   Your Pc has been:    #
+                 #         Hacked!        #
+                 ##########################                        
+)";
     myfile.close();
 
 
